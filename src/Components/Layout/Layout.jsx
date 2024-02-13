3f8f30581204e6d436c267203f78922213891e86
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-12 20:39:23 -0500

    up - bg

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line react/prop-types
 const LayoutC = ({ children }) => (
-	<div className='flex flex-col items-center bg-white dark:bg-black/95 mt-16 pt-6 text-black dark:text-white/90'>
+	<div className='flex flex-col items-center bg-white dark:bg-black/65 mt-16 pt-6 text-black dark:text-white/90'>
 		{children}
 	</div>
 );
