a510fce3522ea8b0cfab96432093e13509f02430
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-16 16:18:47 -0500

    add - checkour side menu _ component

---
+++ src/Components/CheckoutSideMenu/CheckoutSideMenu.jsx
@@ -0,0 +1,27 @@
+import { XMarkIcon } from '@heroicons/react/20/solid';
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context';
+import './styles.css';
+
+const CheckoutSideMenu = () => {
+	const context = useContext(ShoppingCartContext);
+	return (
+		<aside
+			className={`${
+				context.isProductDetailOpen ? 'flex' : 'hidden'
+			} checkout-side-menu flex-col fixed bg-white right-0 dark:bg-[#050505] border border-black dark:border-white rounded-lg overflow-scroll`}
+		>
+			<div className='flex justify-between items-center p-5'>
+				<h2 className='font-medium text-xl'>My Order</h2>
+				<div>
+					<XMarkIcon
+						className='h-6 w-6 text-black dark:text-white cursor-pointer hover:text-red-500 dark:hover:text-red-500 hover:scale-125 hover:duration-150'
+						onClick={context.closeProductDetail}
+					/>
+				</div>
+			</div>
+		</aside>
+	);
+};
+
+export default CheckoutSideMenu;
