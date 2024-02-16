668f43c25087eff3d1e29108145def4d66400b2c
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-16 18:08:10 -0500

    add - order card component

---
+++ src/Components/OrderCard/OrderCard.jsx
@@ -0,0 +1,25 @@
+import { XMarkIcon } from '@heroicons/react/24/solid';
+
+// eslint-disable-next-line react/prop-types
+const OrderCard = ({ title, imageURL, price }) => {
+	return (
+		<div className='flex justify-between items-center'>
+			<div className='flex items-center gap-2'>
+				<figure className='w-20 h-20'>
+					<img
+						src={imageURL}
+						alt={title}
+						className='w-full h-full rounded-lg object-cover'
+					/>
+				</figure>
+				<p className='text-sm font-normal'>{title} </p>
+			</div>
+			<div>
+				<p className='text-lg font-medium'>{price}</p>
+				<XMarkIcon className='h-6 w-6 cursor-pointer' />
+			</div>
+		</div>
+	);
+};
+
+export default OrderCard;
