7e41a5f448b35c5ebd090290a57f385dad0e2966
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-16 15:31:13 -0500

    add - product detail card ind

--- src/Components/ProductDetail/ProductDetail.jsx
+++ src/Components/ProductDetail/ProductDetail.jsx
@@ -17,6 +17,24 @@ const ProductDetail = () => {
 					<XMarkIcon className='h-6 w-6 text-black dark:text-white' />
 				</div>
 			</div>
+			<figure className='px-6'>
+				<img
+					className='w-full h-full rounded-lg'
+					src={context.productToShow.images[0]}
+					alt={context.productToShow.title}
+				/>
+			</figure>
+			<p className='flex flex-col p-6'>
+				<span className='font-medium text-2xl mb-2'>
+					${context.productToShow.price}
+				</span>
+				<span className='font-medium text-lg'>
+					${context.productToShow.title}
+				</span>
+				<span className='font-light text-sm'>
+					${context.productToShow.description}
+				</span>
+			</p>
 		</aside>
 	);
 };
