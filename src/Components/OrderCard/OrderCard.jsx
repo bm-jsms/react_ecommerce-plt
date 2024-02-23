945aa36ecd2a5e6412dc54e975f5cd5fd06a8b2f
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-22 19:21:01 -0500

    add: checkout products

--- src/Components/OrderCard/OrderCard.jsx
+++ src/Components/OrderCard/OrderCard.jsx
@@ -2,6 +2,15 @@ import { XMarkIcon } from '@heroicons/react/24/solid';
 
 // eslint-disable-next-line react/prop-types
 const OrderCard = ({ id, title, imageURL, price, handleDelete }) => {
+	let renderXMarkIcon;
+	if (handleDelete) {
+		renderXMarkIcon = (
+			<XMarkIcon
+				className='h-6 w-6 cursor-pointer hover:scale-125 hover:text-red-500 dark:hover:text-red-500 duration-150'
+				onClick={() => handleDelete(id)}
+			/>
+		);
+	}
 	return (
 		<div className='flex justify-between items-center text-black dark:text-white mb-2'>
 			<div className='flex items-center gap-2'>
@@ -16,10 +25,7 @@ const OrderCard = ({ id, title, imageURL, price, handleDelete }) => {
 			</div>
 			<div className='flex items-center gap-2'>
 				<p className='text-lg font-medium'>${price}</p>
-				<XMarkIcon
-					className='h-6 w-6 cursor-pointer hover:scale-125 hover:text-red-500 dark:hover:text-red-500 duration-150'
-					onClick={() => handleDelete(id)}
-				/>
+				{renderXMarkIcon}
 			</div>
 		</div>
 	);
--- src/Pages/MyOrder/MyOrder.jsx
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -1,6 +1,26 @@
 import LayoutC from '../../Components/Layout/Layout';
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context/index';
+import OrderCard from '../../Components/OrderCard/OrderCard';
+
 const MyOrder = () => {
-	return <LayoutC>My Order</LayoutC>;
+	const context = useContext(ShoppingCartContext);
+	return (
+		<LayoutC>
+			My Order
+			<div className='flex flex-col w-80'>
+				{context.order?.slice(-1)[0].products.map(product => (
+					<OrderCard
+						key={product.id}
+						id={product.id}
+						title={product.title}
+						imageURL={product.images}
+						price={product.price}
+					/>
+				))}
+			</div>
+		</LayoutC>
+	);
 };
 
 export default MyOrder;
