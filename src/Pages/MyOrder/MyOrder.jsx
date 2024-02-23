975b1c1a9d07d5136aa7aeccd879e3368af6bbfd
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-22 21:53:27 -0500

    add: checkout products - order

--- src/Pages/MyOrder/MyOrder.jsx
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -7,6 +7,11 @@ import { Link } from 'react-router-dom';
 
 const MyOrder = () => {
 	const context = useContext(ShoppingCartContext);
+	const currentPath = window.location.pathname;
+	let index = currentPath.substring(currentPath.lastIndexOf('/') + 1);
+
+	if (index === 'last') index = context.order?.length - 1;
+
 	return (
 		<LayoutC>
 			<div className='flex w-80 items-center relative justify-center'>
@@ -16,7 +21,7 @@ const MyOrder = () => {
 				<h1>My Order</h1>
 			</div>
 			<div className='flex flex-col w-80'>
-				{context.order?.slice(-1)[0].products.map(product => (
+				{context.order?.[index]?.products.map(product => (
 					<OrderCard
 						key={product.id}
 						id={product.id}
