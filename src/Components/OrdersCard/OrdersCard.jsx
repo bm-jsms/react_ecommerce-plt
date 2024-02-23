1193efb08a7e5cdaba23891d93b3b73d59d0a053
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-22 22:00:22 -0500

    up: styles

--- src/Components/OrdersCard/OrdersCard.jsx
+++ src/Components/OrdersCard/OrdersCard.jsx
@@ -1,13 +1,13 @@
-import { XMarkIcon } from '@heroicons/react/24/solid';
-
 // eslint-disable-next-line react/prop-types
 const OrdersCard = ({ totalPrice, totalProducts }) => {
 	return (
-		<div className='flex justify-between items-center text-black dark:text-white mb-2 border border-black'>
-			<p>
-				<span>01.01.2024</span>
-				<span>{totalProducts}</span>
-				<span>{totalPrice}</span>
+		<div className='flex justify-between items-center text-black dark:text-white border border-black w-80 p-4 rounded-lg mb-4'>
+			<p className='flex justify-between'>
+				<div className='flex flex-col'>
+					<span className='font-normal'>01.01.2024</span>
+					<span className='font-normal'>{totalProducts}articles</span>
+				</div>
+				<span className='font-medium text-2xl'>${totalPrice}</span>
 			</p>
 		</div>
 	);
